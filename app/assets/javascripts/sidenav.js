b93468d2fe564083455416feec56c189d3f42cea
Frederik Wille <frederik@wille.hamburg>
2016-12-12 20:21:03 +0100

    add materialize and font-awesome
    add [materialize-sass](https://github.com/mkhairi/materialize-sass) gem and [font-awesome-rails](https://github.com/bokmann/font-awesome-rails)
    configures assets to use materialize front-end framework

---
+++ app/assets/javascripts/sidenav.js
@@ -0,0 +1,8 @@
+// initialize Sidenav of materialize framework
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
